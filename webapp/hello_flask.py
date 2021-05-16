5ceefbdeaa1b32c280a857847cd3034addf44912
Nistratov <nva@admport.by>
2021-05-16 21:41:37 +0300

    chapter5-web

--- webapp/hello_flask.py
+++ webapp/hello_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -11,7 +11,7 @@ def hello() -> str:
 
 @app.route('/search4')
 def do_search() -> str:
-    return str(search4letters('life, the universe, and everything', 'eiru!'))
+    return str(search4letters('life, the universe, and everything', 'eiru,!'))
 
 
 app.run()
