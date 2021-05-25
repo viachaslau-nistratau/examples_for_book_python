e0ea9fe2195fc24c0f1038a2d7d1260f8c54ad23
Vyacheslav <nva@admport.by>
2021-05-25 19:18:19 +0300

    chapter_9

--- webapp/vsearch4web.py
+++ webapp/vsearch4web.py
@@ -1,37 +1,25 @@
 
 from flask import Flask, render_template, request, escape
 from vsearch import search4letters
-import mysql.connector
+
+from DBcm import UseDatabase
 
 app = Flask(__name__)
 
-
-# def log_request(req: 'flask_request', res: str) -> None:
-#     """
-#     функция записи (добавления) в конец файла vsearch.log (значение 'a')
-#     """
-#     with open('vsearch.log', 'a') as log:
-#         # print(req.form, file=log, end='|')         # данные из HTML-формы веб-приложения
-#         # print(req.remote_addr, file=log, end='|')  # IP-адрес веб-браузера, приславшего форму
-#         # print(req.user_agent, file=log, end='|')   # строка, идентифицирующая браузер пользовател
-#         # print(res, file=log)
-#         print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+# добавляем словарь с параметрами соединения
+app.config['dbconfig'] = {'host': '127.0.0.1',
+                        'user': 'viachaslau_nistratau',
+                        'password': 'Val02061970!',
+                        'database': 'vsearchlogDB', }
 
 
 def log_request(req: 'flask_request', res: str) -> None:
     """
     журналирует веб-запрос и возвращает результаты
     """
-    # определяем параметры соединения
-    dbconfig = {'host': '127.0.0.1',
-                'user': 'viachaslau_nistratau',
-                'password': 'Val02061970!',
-                'database': 'vsearchlogDB', }
-    # - импортируем драйвер, устанавливаем соединение и создаем курсор
-    # import mysql.connector
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-    # создаем строку с текстом запроса ждя записи в БД
+    # исправили код, используя диспетчер контекста 'UseDatabase',
+    # которому передали настройки из app.config
+    with UseDatabase(app.config['dbconfig']) as cursor:
     _SQL = """insert into log
         (phrase, letters, ip, browser_string, results) 
         values
@@ -43,14 +31,14 @@ def log_request(req: 'flask_request', res: str) -> None:
                           req.remote_addr,
                           req.user_agent.browser,
                           res, ))
-    # после записи данных убираем за собой, закрыв курсор и соединение
-    conn.commit()
-    cursor.close()
-    conn.close()
 
 
 @app.route('/search4', methods=['POST'])
 def do_search():  # -> html:
+    """
+    Извлекает данные из запроса;
+    выполняет поиск; возвращает результаты
+    """
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = 'Here are your results:'
@@ -67,6 +55,9 @@ def do_search():  # -> html:
 @app.route('/')
 @app.route('/entry')
 def entry_page():  # -> 'html':
+    """
+    Выводит HTML-форму
+    """
     return render_template('entry.html',
                            the_tittle='Welcome to search4letters on the web!')
 
@@ -74,15 +65,16 @@ def entry_page():  # -> 'html':
 @app.route('/viewlog')
 def view_the_log():  # -> html:
     """
+    Выводит содержимое файлаф журнала в виде HTML-таблицы.
     функция обработки запроса с URL /viewlog
     """
-    contents = []
-    with open('vsearch.log') as log:
-        for line in log:
-            contents.append([])
-            for item in line.split('|'):
-                contents[-1].append(escape(item))
-    titles = ('Form Data', "Remote_addr", 'User_agent', 'Results')
+    with UseDatabase(app.config['dbconfig']) as cursor:
+        _SQL = """select phrase, letters, ip, browser_string, results 
+                  from log"""
+        cursor.execute(_SQL)
+        contents = cursor.fetchall()
+
+    titles = ('Phrase', 'Letters", "Remote_addr", 'User_agent', 'Results')
     return render_template('viewlog.html',
                            the_title='View Log',
                            the_row_titles=titles,
